c9994f9b5156e79e5ea251585622324440ea8b7d
idityaGE <am44910606@gmail.com>
2025-10-14 22:43:41 +0530

    Enhance vesting tests: add employee management and treasury funding functionality

--- anchor/tests/bankrun.spec.ts
+++ anchor/tests/bankrun.spec.ts
@@ -1,10 +1,9 @@
 import {
   PublicKey,
   SystemProgram,
-  Connection
 } from "@solana/web3.js"
 import * as anchor from "@coral-xyz/anchor"
-import { ProgramTestContext, startAnchor, BanksClient } from "solana-bankrun"
+import { ProgramTestContext, startAnchor, BanksClient, Clock } from "solana-bankrun"
 import { BankrunProvider } from "anchor-bankrun"
 import { TOKEN_PROGRAM_ID } from "@solana/spl-token"
 import { createMint, mintTo } from "spl-token-bankrun";
@@ -16,7 +15,7 @@ import NodeWallet from "@coral-xyz/anchor/dist/cjs/nodewallet"
 
 describe("Vesting Program Tests", () => {
   let company_name = "MyCompany"
-  let beneficiary: anchor.web3.Keypair
+  let beneficiary: anchor.web3.Keypair = new anchor.web3.Keypair()
   let context: ProgramTestContext
   let provider: BankrunProvider
   let program: anchor.Program<Vesting>
@@ -31,7 +30,6 @@ describe("Vesting Program Tests", () => {
 
 
   beforeAll(async () => {
-    beneficiary = new anchor.web3.Keypair()
     context = await startAnchor(
       "",
       [{ name: "vesting", programId: new PublicKey(VestingIDL.address) }],
@@ -104,7 +102,78 @@ describe("Vesting Program Tests", () => {
   })
 
   it("Fund Vesting Treasury", async () => {
-    
+    const amount = 10_000 * 10 ** 2;
+    const tx = await mintTo(
+      //@ts-ignore
+      banksClient,
+      employer,
+      mint,
+      treasuryTokenAccount,
+      employer,
+      amount
+    )
+    console.log("Mint Treasury Token Account is Funded : ", tx.logMessages);
   })
+
+  it("Add Employee", async () => {
+    let start_time = new anchor.BN(0);
+    let end_time = new anchor.BN(100);
+    let cliff_time = new anchor.BN(0);
+    let amount = new anchor.BN(100);
+
+    const tx = await program.methods.addEmployee(
+      amount,
+      start_time,
+      cliff_time,
+      end_time
+    )
+      .accounts({
+        beneficiary: beneficiary.publicKey,
+        vestingAccount: vestingAccountKey
+      })
+      .rpc({ commitment: 'confirmed', skipPreflight: true })
+
+    console.log("Add Employee Transaction: ", tx);
+    const employeeVestingData = await program.account.employeeAccount.fetch(employeeAccount, 'confirmed')
+    console.log("Employee Vesting Data: ", employeeVestingData);
+  })
+
+  it("Cliam Offer", async () => {
+    await new Promise((res) => setTimeout(res, 1000))
+    const currentClock = await banksClient.getClock();
+    context.setClock(
+      new Clock(
+        currentClock.slot,
+        currentClock.epochStartTimestamp,
+        currentClock.epoch,
+        currentClock.leaderScheduleEpoch,
+        1000n
+      )
+    )
+
+    let tx = await program2.methods.claimTokens(company_name)
+      .accounts({
+        tokenProgram: TOKEN_PROGRAM_ID
+      })
+      .rpc({ commitment: "confirmed" })
+
+    console.log("Claim Transaction: ", tx);
+  })
+
+  it("Revoke Employee", async () => {
+    const tx = await program.methods.revokeEmployee(company_name)
+      .accounts({
+        beneficiary: beneficiary.publicKey,
+        owner: employer.publicKey
+      })
+      .signers([employer])
+      .rpc({ commitment: "confirmed" })
+
+    console.log("Account Revoked for beneficiary : ", beneficiary.publicKey.toBase58());
+
+    const employeeVestingData = await program.account.employeeAccount.fetch(employeeAccount, 'confirmed')
+    console.log("Revoked Employed Account Details :", employeeVestingData);
+  })
+
 
 })
